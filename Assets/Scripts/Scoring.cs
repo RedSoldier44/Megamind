00b7b3fbebbc76895680c9580c73fd4c088a6dda
RedSoldier44 <75722775+RedSoldier44@users.noreply.github.com>
2025-09-11 12:01:03 +0200

    feat(scoring cs) : update seralized variables

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -4,20 +4,47 @@ public class Scoring : MonoBehaviour
 {
 
     #region Fields
+    //-------------------------------------------------
+
+
     [SerializeField] private float score;
+
+    [SerializeField] private float timer;
+
+    [SerializeField] private float animalButtonScore;
+
+    [SerializeField] private float locationButtonScore;
+
+    [SerializeField] private float voicesButtonScore;
+
+    [SerializeField] private float movementButtonScore;
+
+    [SerializeField] private float birdsButtonScore;
+
+    [SerializeField] private float resetButtonScore;
 
 
     #endregion
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    #region Unity Methods
+    //-------------------------------------------------
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
     }
+
+    #endregion
+
+    #region Custom Methods
+    //-------------------------------------------------
+
+    #endregion
 }
